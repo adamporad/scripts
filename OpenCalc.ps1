5dd949a001d552b0aa31d1c59ef0f003faef765a
Adam Porad <adamporad@users.noreply.github.com>
2019-12-27 13:16:17 -0800

    Open or bring to focus the calculator app
    - Open the Microsoft Calculator app if it is not running, otherwise bring the app into focus
    - Purpose: Remap the "calculator" key on keyboard so that only a single instance of the Calculator app is running.

---
+++ OpenCalc.ps1
@@ -0,0 +1,16 @@
+﻿# Open the Microsoft Calculator app if it is not running, otherwise bring the app into focus
+# Purpose: Remap the "calculator" key on keyboard so that only a single instance of the Calculator app is running.
+
+$calc = Get-Process Calculator -ErrorAction SilentlyContinue
+
+if ($calc)
+{
+    # Bring Calculator into focus
+    Add-Type -AssemblyName Microsoft.VisualBasic
+    [Microsoft.VisualBasic.Interaction]::AppActivate($calc.ProcessName)
+
+}
+else
+{
+     Start-Process calc.exe
+}
